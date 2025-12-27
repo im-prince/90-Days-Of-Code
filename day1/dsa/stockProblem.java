19fdaff0658e5a1f272da250260299284ac223e5
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2025-12-27 20:23:04 +0530

    Q

--- day1/dsa/stockProblem.java
+++ day1/dsa/stockProblem.java
@@ -1,24 +1,28 @@
 public class stockProblem {
-    public static void main(String[] var0) {
-        int[] var1 = new int[]{7, 1, 5, 3, 6, 4,7};
-        System.out.println(maxProfit(var1));
+   public static void main(String[] args) {
+
+
+        int prices[] = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
     }
+    static int maxProfit(int[] prices) {
 
-    static int maxProfit(int[] var0) {
-        int var1 = 0;
-        int var2 = var0[0];
+        int maxProfit = 0;
+        int buyPrice = prices[0];
 
-        for(int var3 = 1; var3 < var0.length; ++var3) {
-            int var4 = var0[var3] - var2;
-            if (var4 > var1) {
-                var1 = var4;
+        for (int i = 1; i <prices.length ; i++) {
+            int currProfit = prices[i] - buyPrice;
+
+            if (currProfit > maxProfit){
+                maxProfit = currProfit;
             }
 
-            if (var0[var3] < var2) {
-                var2 = var0[var3];
+            if (prices[i] < buyPrice){
+                buyPrice = prices[i];
             }
+
         }
+        return maxProfit;
 
-        return var1;
     }
 }
