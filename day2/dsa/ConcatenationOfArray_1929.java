95fa67e14d0d8ff8b5d4deb8bec5fee06670fc83
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2025-12-28 21:49:21 +0530

    day 2 array problem

---
+++ day2/dsa/ConcatenationOfArray_1929.java
@@ -0,0 +1,20 @@
+
+import java.util.Arrays;
+public class ConcatenationOfArray_1929 {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,1};
+        System.out.println(Arrays.toString(getConcatenation(arr)));
+
+    }
+
+    static int[] getConcatenation(int[] nums) {
+        int[] ans = new int[2* nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i+ nums.length] = ans[i];
+
+        }
+        return ans;
+    }
+}
