2588b78defecbed4d6405e2e8e467b1f86e82bab
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2025-12-31 14:41:31 +0530

    drag folder to main project

---
+++ Spring Boot/intoduction/src/main/java/com/springboot/prince/intoduction/IntoductionApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.prince.intoduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntoductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntoductionApplication.class, args);
+	}
+
+}
---
+++ Spring Boot/intoduction/src/test/java/com/springboot/prince/intoduction/IntoductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.prince.intoduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntoductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
