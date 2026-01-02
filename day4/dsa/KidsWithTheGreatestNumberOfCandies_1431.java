abf7650dd8e79d91833f9c9affa3b796801f6881
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2026-01-02 22:19:16 +0530

    Array and recursion problem

---
+++ day4/dsa/KidsWithTheGreatestNumberOfCandies_1431.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class KidsWithTheGreatestNumberOfCandies_1431 {
+    public static void main(String[] args) {
+
+        int[] candies = {2, 3, 5, 1, 3};
+        int extracandies = 3;
+        System.out.println(kidsWithCandies(candies,extracandies));
+    }
+
+
+    static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> ans = new ArrayList<>();
+        int max = 0;
+        //calculating the highest value in the array
+        for(int i = 0 ; i <  candies.length; i ++){
+            if(max < candies[i]){
+                max = candies[i];
+            }
+        }
+        for(int i = 0 ; i < candies.length; i ++){
+            //checking if the sum greater or eqaul to max
+            if(candies[i] + extraCandies >= max){
+                ans.add(true);
+            }else{
+                ans.add(false);
+            }
+        }
+        return ans;
+    }
+}
