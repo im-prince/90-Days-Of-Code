e07a15c842db886587cfd25bf94fb09b690af063
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2026-01-05 23:36:06 +0530

    sum of n natural no

--- day5/dsa/sumOfNnaturalNo.java
+++ day5/dsa/sumOfNnaturalNo.java
@@ -2,4 +2,16 @@ package day5.dsa;
 
 public class sumOfNnaturalNo {
 
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+    }
+
+
+    static int sum(int n){
+        if(n==0){
+            return 0;
+        }
+        return sum(n-1) + n;
+    }
+
 }
