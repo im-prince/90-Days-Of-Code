727711ba188834527e6f959ea1d73ebf0be2fe69
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2026-02-02 13:33:56 +0530

    sumOfNum

---
+++ day7/dsa/sumOfNum.java
@@ -0,0 +1,14 @@
+package day7.dsa;
+
+public class sumOfNum {
+
+    public static void main(String[] args) {
+        int nums[] ={23,45,67,86,42,76};
+        int sum =0;
+        for(int num : nums){
+            sum= sum +num;
+        }
+        System.out.println(sum);
+    }
+    
+}
