776ac85f7eefa225f2cc122f49cdc8c367305aa1
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2026-02-19 12:47:05 +0530

    working with Transition, Transform, and Animation

---
+++ Spring Boot/bootrestbook/src/main/java/com/api/book/bootrestbook/entities/book.java
@@ -0,0 +1,53 @@
+package com.api.book.bootrestbook.entities;
+
+public class book {
+
+
+    private int id;
+    private String author;
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "book{" +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+
+    public book(String author, int id, String title) {
+        this.author = author;
+        this.id = id;
+        this.title = title;
+    }
+
+    public book(){
+    }
+
+}
+
---
+++ Spring Boot/bootrestbook/src/main/java/com/api/book/bootrestbook/services/bookService.java
@@ -0,0 +1,11 @@
+package com.api.book.bootrestbook.services;
+
+import com.api.book.bootrestbook.entities.book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class bookService {
+
+    private static List<book> list = new ArrayList<>();
+}
