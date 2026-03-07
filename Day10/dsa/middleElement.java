0a7ed10a7c81c6b94330a671e42e83716a22baa8
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2026-03-07 20:40:46 +0530

    LL

--- Day10/dsa/middleElement.java
+++ Day10/dsa/middleElement.java
@@ -11,9 +11,11 @@ public class middleElement {
       ListNode(int val) {
           this.val = val;
       }
+
       ListNode(int val, ListNode next) {
           this.val = val; this.next = next;
       }
+
   }
 
     public static ListNode middleNode(ListNode head){
@@ -24,7 +26,6 @@ public class middleElement {
             slow = slow.next;
             fast = fast.next.next;
         }
-
         return slow;
     }
 }
