02e08ca3bec8ad1434c30fef0b2f4f9b7e4e120f
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2026-03-10 18:42:04 +0530

    File Uploading

---
+++ Spring Boot/bootrestbook/src/main/java/com/api/book/bootrestbook/controller/FileUploadController.java
@@ -0,0 +1,57 @@
+package com.api.book.bootrestbook.controller;
+
+import com.api.book.bootrestbook.helper.FileUploadHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class FileUploadController {
+
+    @Autowired
+    private FileUploadHelper fileUploadHelper;
+
+//    @PostMapping("/upload-file")
+//    public ResponseEntity<String> uploadFile(){
+//        return ResponseEntity.ok("working");
+//    }
+
+
+    @PostMapping("/upload-file")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file)
+    {
+//        System.out.println(file.getOriginalFilename());
+//        System.out.println(file.getSize());
+//        System.out.println(file.getContentType());
+//        System.out.println(file.getName());
+
+        try {
+            if (file.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Request must contain file");
+            }
+
+            if (!file.getContentType().equals("image/jpeg")) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Only JPEG content are allowed");
+
+            }
+
+            boolean f = fileUploadHelper.uploadFile(file);
+            if (f){
+                return ResponseEntity.ok("uploaded file successfully");
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("something went wrong please try again");
+
+    }
+
+
+
+}
