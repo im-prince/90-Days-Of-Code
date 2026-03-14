b2cac4200a90643690db34ca06f21a6b04567f7c
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2026-03-14 13:23:00 +0530

    upper bond

---
+++ searching/upperAndLoweBond.java
@@ -0,0 +1,27 @@
+package searching;
+
+
+
+public class upperAndLoweBond {
+    public static void main(String[] args) {
+        int[] a ={2,4,5,67,78,98};
+        int target = 67;
+        System.out.println(upperBond(a,target));
+    }
+
+    static int upperBond(int[] a, int target){
+        int s= 0;
+        int e = a.length-1;
+        int ans = -1;
+        while (s <= e){
+            int m = s+(e-s)/2;
+            if (target < a[m]){
+                ans = m;
+                e = m-1;
+            }if (target >= a[m]){
+                s = m+1;
+            }
+        }
+        return ans;
+    }
+}
