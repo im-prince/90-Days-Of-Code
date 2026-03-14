996fce48769608005b4be9546561c16670037b72
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2026-03-14 13:27:54 +0530

    lower bond

--- searching/upperAndLoweBond.java
+++ searching/upperAndLoweBond.java
@@ -5,8 +5,8 @@ package searching;
 public class upperAndLoweBond {
     public static void main(String[] args) {
         int[] a ={2,4,5,67,78,98};
-        int target = 67;
-        System.out.println(upperBond(a,target));
+        int target = 98;
+        System.out.println(lowerBond(a,target));
     }
 
     static int upperBond(int[] a, int target){
@@ -21,6 +21,24 @@ public class upperAndLoweBond {
             }if (target >= a[m]){
                 s = m+1;
             }
+        }
+        return ans;
+    }
+
+
+    static int lowerBond(int[] a, int target){
+        int s = 0;
+        int e = a.length-1;
+        int ans = -1;
+
+        while (s <= e){
+            int m = s+(e-s)/2;
+
+            if (target > a[m]){
+                ans = m;
+                s = m+1;
+
+            }else e = m-1;
         }
         return ans;
     }
