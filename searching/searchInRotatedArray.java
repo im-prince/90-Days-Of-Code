150ab320e17cf78647dec21548998595a0a3de9b
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2026-03-19 10:33:21 +0530

    search in rotated Array

---
+++ searching/searchInRotatedArray.java
@@ -0,0 +1,42 @@
+package searching;
+
+import java.util.Arrays;
+
+public class searchInRotatedArray {
+    public static void main(String[] args) {
+
+
+        int[] a = {10,11,13,14,1,2,4,6,7,9};
+        System.out.println(rotatedArray(a,6));
+
+    }
+
+
+    static int rotatedArray(int[] a, int target){
+        int l = 0;
+        int r = a.length -1;
+        while(l <= r){
+            int m = l+(r-l)/2;
+            if(a[m] == target) return m;
+            if (a[m] == a[l] || a[m] == a[r]){
+                l++;
+                r--;
+            } else if (a[l] <= a[m]) {
+                if (a[l] <= target && target < a[m]){
+                    r = m-1;
+                }else {
+                    l= m+1;
+                }
+
+            }else{
+                if (a[m] < target && target <= a[r]){
+                    l= m+1;
+                }else {
+                    r= m-1;
+                }
+            }
+        }
+        return -1;
+    }
+
+}
