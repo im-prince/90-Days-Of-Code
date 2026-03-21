90e11c3b73c1d79a25c3f7fb8767c7cb61f16de8
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2026-03-21 16:41:03 +0530

    Height Of BT

---
+++ binaryTree/heightOfBT.java
@@ -0,0 +1,36 @@
+package binaryTree;
+
+public class heightOfBT {
+    public static void main(String[] args) {
+        Node n1 = new Node(15);
+        Node n2 = new Node(9);
+        Node n3 = new Node(20);
+        Node n4 = new Node(15);
+        Node n5 = new Node(7);
+
+        Node root = n1;
+        n1.left = n2;
+        n1.right = n3;
+        n3.left = n4;
+        n4.right = n5;
+
+        System.out.println(heightBT(root));
+
+    }
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static int heightBT(Node root){
+        if (root == null) return 0;
+        int hl = heightBT(root.left);
+        int hr = heightBT(root.right);
+        return Math.max(hl,hr) +1;
+    }
+}
