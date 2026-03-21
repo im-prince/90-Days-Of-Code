79811095b7b44ad37a1c5e71d4a9a6f1a228dac5
Prince Kumar <114931770+im-prince@users.noreply.github.com>
2026-03-21 15:43:57 +0530

    Binart Tree - inOrder

---
+++ binaryTree/traversal.java
@@ -0,0 +1,43 @@
+package binaryTree;
+
+import org.w3c.dom.Node;
+
+public class traversal {
+    public static void main(String[] args) {
+        Node n1 = new Node(3);
+        Node n2 = new Node(9);
+        Node n3 = new Node(20);
+        Node n4 = new Node(15);
+        Node n5 = new Node(7);
+
+        Node root = n1;
+        n1.left = n2;
+        n1.right = n3;
+        n3.left = n4;
+        n3.right = n5;
+
+        inOrder(root);
+
+    }
+
+    static void inOrder(Node root){
+
+        if(root == null) return;
+        inOrder(root.left);
+        System.out.print(root.data +" ");
+        inOrder(root.right);
+
+    }
+
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+}
